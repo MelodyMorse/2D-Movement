fdd83945d0f1e3247e3c28f000770912ffc7858e
Melody Morse <14865089+MelodyMorse@users.noreply.github.com>
2019-10-11 17:04:04 -0400

    first test commit

--- Assets/Scripts/JumpArc.cs
+++ Assets/Scripts/JumpArc.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class JumpArc : MonoBehaviour
 {
+    LineRenderer _lineRenderer;
+    public float arcHeight = 2;
+    public float arcDistance = 3;
+    public int numberPoints = 3;
+    private float _initialVelocity;
+    private float _finalVelocity;
+    private float _acceleration;
+    private float _displacement; 
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        //Debug.Log("sq rt 120: " + Mathf.Sqrt(120));
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //_initialVelocity = CalculateInitialVelocity(); 
+
+        _lineRenderer.positionCount = numberPoints;
+        _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.SetPosition(1, transform.position + new Vector3(arcDistance * 0.5f, arcHeight, 0));
+        _lineRenderer.SetPosition(2, transform.position + new Vector3(arcDistance, 0, 0));
+        // y = a x^2 + arcHeight
+
+        float g = Physics.gravity.magnitude; // get the gravity value
+        float vSpeed = Mathf.Sqrt(2 * g * arcHeight); // calculate the vertical speed
+        float totalTime = 2 * vSpeed / g; // calculate the total time
+        float hSpeed = arcDistance / totalTime; // calculate the horizontal speed
+        //rigidbody.velocity = Vector3(hSpeed, vSpeed, 0); // launch the projectile!
     }
 
     // Update is called once per frame
@@ -15,4 +43,15 @@ public class JumpArc : MonoBehaviour
     {
         
     }
+
+    public static float CalculateInitialVelocity(float finalVelocity, float acceleration, float displacement)
+    {
+        float initialVelocity = 0.0f;
+
+        initialVelocity = Mathf.Sqrt(finalVelocity * finalVelocity - 2 * acceleration * displacement);
+
+        return initialVelocity;
+    }
+
+
 }
---
+++ Assets/Tests/JumpArcTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace Tests
+{
+    public class JumpArcTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Calculate_Initial_Velocity_Test()
+        {
+            float expected = 10.95445f;
+            float actual = 420;
+            float _finalVelocity = 0;
+            float _displacement = 3;
+            float _acceleration = -20;
+            actual = JumpArc.CalculateInitialVelocity(_finalVelocity, _displacement, _acceleration);
+
+            //actual = JumpArc
+            // Use the Assert class to test conditions
+            //Assert.AreEqual(expected, actual);
+            float tolerance = 0.00001f;
+            //int magnitude = 1 + (expected == 0.0 ? -1 : Convert.ToInt32(Math.Floor(Math.Log10(expected))));
+            //int precision = 15 - magnitude;
+
+            //double tolerance = 1.0 / Math.Pow(10, precision);)
+
+            Assert.That(actual, Is.EqualTo(expected).Within(tolerance));
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+       /* [UnityTest]
+        public IEnumerator JumpArcTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        } */
+    }
+}
